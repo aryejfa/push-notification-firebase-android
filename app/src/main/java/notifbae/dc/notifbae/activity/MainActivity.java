a6ff4ad6ed97a9b19344950c5f7c4c539f1faac8
Ary EJFA <36819538+aryejfa@users.noreply.github.com>
2021-08-19 17:41:52 +0700

    Update MainActivity.java

--- app/src/main/java/notifbae/dc/notifbae/activity/MainActivity.java
+++ app/src/main/java/notifbae/dc/notifbae/activity/MainActivity.java
@@ -12,7 +12,6 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import com.google.firebase.messaging.FirebaseMessaging;
 
 import notibae.dc.notibae.R;
